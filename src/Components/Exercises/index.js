c870b10c0207f978d62ef5a452baa2e4814373e4
takahirosawamura <cawst222@gmail.com>
2018-10-03 15:26:33 +0900

    add Grid Layout

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import LeftPane from './LeftPane'
+import RightPane from './RightPane'
+
+const styles = {
+  Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
+}
 
 export default props =>
-  <div>
-    Hello from Exercises
-  </div>
+  <Grid container sm>
+    <Grid item sm>
+      <LeftPane styles={styles}/>
+    </Grid>
+    <Grid item sm>
+      <RightPane styles={styles}/>
+    </Grid>
+  </Grid>
